6704fbf98a6f3cdac44046cdfdc9e280f1b5de71
Paul Grogan <paul@ptgrogan.com>
2022-12-03 15:15:10 -0500

    expose ssl configuration for broker and backend

--- app/tatc_app/worker.py
+++ app/tatc_app/worker.py
@@ -1,16 +1,53 @@
 from celery import Celery
 from skyfield.api import load
 import os
+import ssl
 from dotenv import load_dotenv
 
 # Load environment variables from the .env file
 load_dotenv()
 
+# parse the broker connection
+broker_string = os.getenv("TATC_BROKER", "amqp://localhost:5672//")
+if "amqps://" in broker_string:
+    broker_ssl_option = os.getenv("TATC_BROKER_SSL_CERT_REQS", "NONE")
+    broker_ssl_config = {
+        'ssl_keyfile': os.getenv("TATC_BROKER_SSL_KEYFILE", None),
+        'ssl_certfile': os.getenv("TATC_BROKER_SSL_CERTFILE", None),
+        'ssl_ca_certs': os.getenv("TATC_BROKER_SSL_CA_CERTS", None),
+        'ssl_cert_reqs': (
+            ssl.CERT_REQUIRED if broker_ssl_option == "REQUIRED"
+            else ssl.CERT_OPTIONAL if broker_ssl_option == "OPTIONAL"
+            else ssl.CERT_NONE
+        )
+    }
+else:
+    broker_ssl_config = False
+
+# parse the backend connection
+backend_string = os.getenv("TATC_BACKEND", "redis://localhost:6379/")
+if "rediss://" in backend_string:
+    backend_ssl_option = os.getenv("TATC_BACKEND_SSL_CERT_REQS", "NONE")
+    backend_ssl_config = {
+        'ssl_keyfile': os.getenv("TATC_BACKEND_SSL_KEYFILE", None),
+        'ssl_certfile': os.getenv("TATC_BACKEND_SSL_CERTFILE", None),
+        'ssl_ca_certs': os.getenv("TATC_BACKEND_SSL_CA_CERTS", None),
+        'ssl_cert_reqs': (
+            ssl.CERT_REQUIRED if backend_ssl_option == "REQUIRED"
+            else ssl.CERT_OPTIONAL if backend_ssl_option == "OPTIONAL"
+            else ssl.CERT_NONE
+        )
+    }
+else:
+    backend_ssl_config = False
+
 # Create the Celery app
 app = Celery(
     "tatc_app",
-    broker=os.getenv("TATC_BROKER", "amqp://localhost:5672//"),
-    backend=os.getenv("TATC_BACKEND", "redis://localhost:6379/"),
+    broker=broker_string,
+    broker_use_ssl=broker_ssl_config,
+    backend=backend_string,
+    redis_backend_use_ssl=backend_ssl_config,
     include=[
         "tatc_app.coverage.tasks",
         "tatc_app.generation.tasks",
